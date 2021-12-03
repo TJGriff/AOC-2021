7dece07c0ba94d2c7d3bfe80f4195b8dc75df265
Toby Griffiths <toby.griffiths@ba.com>
2021-12-03 09:27:32 +0000

    Answer to part 2

--- d2_answers.py
+++ d2_answers.py
@@ -24,27 +24,24 @@ a1 = x*y
 
 ### PART 2
 
-# Import and split lines
-d2 = open('Day 2 Data.txt', 'r').read().split('\n')
-
-movements = [x.split()[0] for x in d2]
-size = [int(x.split()[1]) for x in d2]
-
 # Let x represent the position and y represent the depth
 # Starting positions
 x = 0
 y = 0
+z = 0
 
 
 for i in range(len(size)):
     if movements[i] == 'forward':
         x += size[i]
+        y += (z * size[i])
     elif movements[i] == 'down':
-        y += size[i]
+        z += size[i]
     elif movements[i] == 'up':
-        y -= size[i]
+        z -= size[i]
     else:
         raise ValueError('Movement not recognised')
+    print(x,y,z)
 
 
 
